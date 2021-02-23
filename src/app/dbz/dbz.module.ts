4910e2491dd7366db60af50635f7e5b407faa025
Jeroham Miranda <verdizone@gmail.com>
2021-02-23 14:21:57 -0600

    59.Crear componentes hijos

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent],
   exports:[
     MainPageComponent
   ],
